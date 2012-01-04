cd11598951fe84e775d78ddaa682528c3c7f0c84
NotoriousRGB <info@notoriousrgb.com>
2012-01-04 17:14:42 +1100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '37d720400e0348a4b82bd4ae7294d821c410f90fa591a05345c9907fd90846cd56a0b0cd869d57086084bb583d0715a7480d5990a4a3673724017f6c805483d9'
