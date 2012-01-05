206ba4d8ee6ff2d94ba86082ff7bb2d2b0078a42
NotoriousRGB <info@notoriousrgb.com>
2012-01-05 16:24:55 +1100

    Blah

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/about"
-  get "pages/contact"
+ 
+  root :to => 'pages#home'
+ 
+  match '/about',   :to => 'pages#about'
+  match '/contact', :to => 'pages#contact'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
